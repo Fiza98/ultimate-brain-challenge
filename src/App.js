70aa449ca7743f14e69223aaffb663d8e7775b36
Afriza <70303801+Fiza98@users.noreply.github.com>
2025-02-19 22:58:08 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import UltimateBrainChallenge from "./components/UltimateBrainChallenge";
+import UltimateBrainChallenge from "./UltimateBrainChallenge";
 
 function App() {
-  return <UltimateBrainChallenge />;
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <UltimateBrainChallenge />
+    </div>
+  );
 }
 
 export default App;
